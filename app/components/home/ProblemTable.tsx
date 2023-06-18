9bde4865b1f69ab2144ab648e1883e88f9b1d257
AnimeFlix007 <devjitbose111@gmail.com>
2023-06-18 15:45:04 +0530

    problem table with loading && youtube solution

---
+++ app/components/home/ProblemTable.tsx
@@ -0,0 +1,111 @@
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { BsCheckCircle } from "react-icons/bs";
+import { AiFillYoutube } from "react-icons/ai";
+import { IoClose } from "react-icons/io5";
+import { problems } from "@/mocks/Problems";
+import YouTube from "react-youtube";
+
+type ProblemsTableProps = {
+  setLoadingProblems: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const ProblemsTable: React.FC<ProblemsTableProps> = ({
+  setLoadingProblems
+}) => {
+  const [youtubePlayer, setYoutubePlayer] = useState({
+    isOpen: false,
+    videoId: "",
+  });
+  const closeModal = () => {
+    setYoutubePlayer({ isOpen: false, videoId: "" });
+  };
+
+  useEffect(() => {
+    const handleEsc = (e: KeyboardEvent) => {
+      if (e.key === "Escape") closeModal();
+    };
+    window.addEventListener("keydown", handleEsc);
+
+    return () => window.removeEventListener("keydown", handleEsc);
+  }, []);
+  return (
+    <>
+      <tbody className="text-black">
+        {problems.map((problem, idx) => {
+          const difficulyColor =
+            problem.difficulty === "Easy"
+              ? "text-dark-green-s"
+              : problem.difficulty === "Medium"
+              ? "text-dark-yellow"
+              : "text-dark-pink";
+          return (
+            <tr
+              className={`${idx % 2 == 1 ? "bg-dark-layer-1" : ""}`}
+              key={problem.id}
+            >
+              <th className="px-2 py-4 font-medium whitespace-nowrap text-dark-green-s">
+                {/* {solvedProblems.includes(problem.id) && ( */}
+                <BsCheckCircle fontSize={"18"} width="18" />
+                {/* )} */}
+              </th>
+              <td className="px-6 py-4">
+                <Link
+                  className="hover:text-blue-600 cursor-pointer"
+                  href={`/problems/${problem.id}`}
+                >
+                  {problem.title}
+                </Link>
+              </td>
+              <td className={`px-6 py-4 ${difficulyColor}`}>
+                {problem.difficulty}
+              </td>
+              <td className={"px-6 py-4"}>{problem.category}</td>
+              <td className={"px-6 py-4"}>
+                {problem.videoId ? (
+                  <AiFillYoutube
+                    fontSize={"28"}
+                    className="cursor-pointer hover:text-red-600"
+                    onClick={() =>
+                      setYoutubePlayer({
+                        isOpen: true,
+                        videoId: problem.videoId as string,
+                      })
+                    }
+                  />
+                ) : (
+                  <p className="text-gray-400">Coming soon</p>
+                )}
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+      {youtubePlayer.isOpen && (
+        <tfoot className="fixed top-0 left-0 h-screen w-screen flex items-center justify-center">
+          <div
+            className="bg-black z-10 opacity-70 top-0 left-0 w-screen h-screen absolute"
+            onClick={closeModal}
+          ></div>
+          <div className="w-full z-50 h-full px-6 relative max-w-4xl">
+            <div className="w-full h-full flex items-center justify-center relative">
+              <div className="w-full relative">
+                <IoClose
+                  fontSize={"35"}
+                  className="cursor-pointer absolute -top-16 right-0"
+                  onClick={closeModal}
+                />
+                <YouTube
+                  videoId={youtubePlayer.videoId}
+                  loading="lazy"
+                  iframeClassName="w-full min-h-[500px]"
+                />
+              </div>
+            </div>
+          </div>
+        </tfoot>
+      )}
+    </>
+  );
+};
+export default ProblemsTable;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,79 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import ProblemsTable from "./components/home/ProblemTable";
+
 export default function Home() {
+  const [loadingProblems, setLoadingProblems] = useState(true);
+
+  useEffect(() => {
+    const timeOutId = setTimeout(() => {
+      setLoadingProblems(false);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeOutId);
+    };
+  }, []);
+
   return (
-    <p>hello</p>
-  )
+    <>
+      <main className="bg-dark-layer-2 min-h-screen">
+        <h1
+          className="text-2xl text-center text-gray-700 dark:text-gray-400 font-medium
+					uppercase mt-10 mb-5"
+        >
+          Learn DSA in the Best Way
+        </h1>
+        <div className="relative overflow-x-auto mx-auto px-6 pb-10">
+          <table className="text-sm text-left text-gray-500 dark:text-gray-400 sm:w-7/12 w-full max-w-[1200px] mx-auto">
+            {!loadingProblems && (
+              <thead className="text-xs text-gray-700 uppercase dark:text-gray-400 border-b ">
+                <tr>
+                  <th scope="col" className="px-1 py-3 w-0 font-medium">
+                    Status
+                  </th>
+                  <th scope="col" className="px-6 py-3 w-0 font-medium">
+                    Title
+                  </th>
+                  <th scope="col" className="px-6 py-3 w-0 font-medium">
+                    Difficulty
+                  </th>
+
+                  <th scope="col" className="px-6 py-3 w-0 font-medium">
+                    Category
+                  </th>
+                  <th scope="col" className="px-6 py-3 w-0 font-medium">
+                    Solution
+                  </th>
+                </tr>
+              </thead>
+            )}
+            {!loadingProblems && (
+              <ProblemsTable setLoadingProblems={setLoadingProblems} />
+            )}
+          </table>
+          {loadingProblems && (
+            <section className="max-w-[1200px] mx-auto sm:w-7/12 w-full animate-pulse">
+              {[...Array(10)].map((_, idx) => (
+                <LoadingSkeleton key={idx} />
+              ))}
+            </section>
+          )}
+        </div>
+      </main>
+    </>
+  );
 }
+
+const LoadingSkeleton = () => {
+  return (
+    <div className="flex items-center space-x-12 mt-4 px-6">
+      <span className="w-6 h-6 shrink-0 rounded-full  bg-slate-400"></span>
+      <span className="h-4 sm:w-52  w-32  rounded-full  bg-slate-400"></span>
+      <span className="h-4 sm:w-52  w-32 rounded-full  bg-slate-400"></span>
+      <span className="h-4 sm:w-52 w-32 rounded-full  bg-slate-400"></span>
+      <span className="h-4 sm:w-52 w-32 rounded-full  bg-slate-400"></span>
+    </div>
+  );
+};
